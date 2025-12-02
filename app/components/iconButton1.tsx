6408546254d3bf9160a5fb365070354db5a162e8
Aman-Dhakad07 <dhakadaman035@gmail.com>
2025-12-02 14:13:03 +0530

    fixed the height of the sidebar

--- app/components/iconButton1.tsx
+++ app/components/iconButton1.tsx
@@ -37,7 +37,7 @@ export default function SidebarBtn1({
             <Link
                 href={href}
                 onClick={onClick}
-                className={`${ btnTw || "h-[32px] px-3 py-2"} rounded-lg flex items-center gap-[12px] justify-between ${
+                className={`${ btnTw || "h-fit px-3 py-2"} rounded-lg flex items-center gap-[12px] justify-between ${
                     isActive
                         ? "bg-[#285295] text-white"
                         : "bg-transparent text-[#C2CBDE] hover:bg-[#536893] hover:text-white"
